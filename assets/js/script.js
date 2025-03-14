08515a0af1f181e8c09497fd6ccbb946081bb485
Subhadeep718 <subhadeep.99graphicdesigner@gmail.com>
2025-03-14 13:33:52 +0530

    work section

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,17 +47,23 @@
 
 $('.owl-carousel').owlCarousel({
   loop:true,
-  margin:10,
-  nav:true,
+  margin: 0,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
   responsive:{
       0:{
           items:1
       },
       600:{
-          items:2
+          items:1
       },
       1000:{
           items:2
       }
   }
 })
+
+
